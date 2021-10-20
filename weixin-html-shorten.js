2c1cba0bc84ffd098b9da1b881e3ae85999012c5
loaxsc <loaxs.chen@gmail.com>
2021-10-20 14:06:52 +0800

    upload weixin-html-shorten.js

--- weixin-html-shorten.js
+++ weixin-html-shorten.js
@@ -1,3 +1,69 @@
+var shrinkElement = function(elem) { // {{{
+  if ( elem.childElementCount == 0 ) {
+    return 0;
+  }
+
+  if ( elem.childElementCount > 1 ) {
+    for ( let i=0; i < elem.childElementCount; i++ ) {
+      shrinkElement(elem.children[i]);
+    }
+    return 0;
+  }
+
+  let ec = elem.children[0];
+  if ( ec.tagName == elem.tagName ) {
+    while ( ec.childElementCount == 1 && ec.childNodes.length == 1 && ec.tagName == ec.children[0].tagName ) {
+      ec = ec.children[0];
+    }
+    shrinkElement(ec);
+    elem.innerHTML = ec.innerHTML;
+    return 0;
+  } else {
+    shrinkElement(ec);
+  }
+} // */}}}
+
+var insertElements = function() { // {{{
+  // Get date: dt
+  var dt = document.getElementById('publish_time').innerText.replaceAll('-','');
+  var author = document.querySelector('meta[name="author"]');
+  var tt = document.querySelector('meta[property="og:title"]');
+  tt.setAttribute('property','title');
+  tt.content = dt + '-' + tt.content.replace(/\s*\|\s*/g,'：').replace('?','？');
+
+  var d = document.createElement('meta');
+  d.name = 'org:url';
+  d.content = location.toString();
+  nr.insertBefore(d,nr.firstElementChild);
+  nr.insertBefore(author,nr.firstElementChild);
+  nr.insertBefore(tt,nr.firstElementChild);
+
+  var h1 = document.createElement('h1');
+  var a = document.createElement('a');
+  a['href'] = location.toString();
+  a.textContent = document.getElementById('activity-name').innerText;
+  h1.append(a);
+
+  var p = document.createElement('p');
+  p.textContent = dt;
+  p.id = 'datetime';
+  nr.insertBefore(p,nr.firstElementChild);
+  nr.insertBefore(h1,nr.firstElementChild);
+
+  if ( document.querySelector('.article_modify_area') != null ) {
+    nr.append(document.querySelector('.article_modify_area'));
+    document.querySelector('.article_modify_area').outerHTML = document.querySelector('.article_modify_area').outerHTML.replaceAll('div','p');
+    document.querySelector('.article_modify_area').id = 'article_modify_area';
+    document.querySelector('#article_modify_area').removeAttribute('class');
+  }
+
+  d = document.createElement('p');
+  d.id = 'org_url';
+  d.innerHTML = '<a href="{}">原始网址</a>'.replace('{}',document.querySelector('meta[name="org:url"]').content);
+  nr.appendChild(d);
+} // }}}
+
+
 setTimeout(function() {
   var eventFire = function(el, etype) {
 	if (el.fireEvent) {
@@ -56,4 +122,9 @@ setTimeout(function() {
 	  a.href = a.href.replace(/&(chksm|scene).+/,'');
 	}
   });
+
+  var nr = document.querySelector('#js_content');
+  shrinkElement(nr);
 },1000);
+
+// vim: fdm=marker sw=2
