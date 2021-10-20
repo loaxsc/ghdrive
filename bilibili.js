6e5bf6f8ca92e4d0eb8435ee4af1db977cc9cca8
loaxsc <loaxs.chen@gmail.com>
2021-10-20 15:07:48 +0800

    upload bilibili.js

--- bilibili.js
+++ bilibili.js
@@ -17,7 +17,9 @@ var bilibili_btn_func = function(e) {
   });
 }
 btn.addEventListener('click', bilibili_btn_func);
-document.querySelector('h1').after(btn);
+setTimeout(function() {
+  document.querySelector('h1').after(btn);
+}, 1000);
 //rq.append(btn);
 
 // vim: fdm=marker sw=2
