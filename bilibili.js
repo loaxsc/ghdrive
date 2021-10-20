f08a417b98e6d8647cb925dfb0ee579be2b66eae
loaxsc <loaxs.chen@gmail.com>
2021-10-20 17:04:25 +0800

    upload bilibili.js

--- bilibili.js
+++ bilibili.js
@@ -1,7 +1,7 @@
 var btn = document.createElement('input');
 btn.setAttribute('id','bilibili_btn');
 btn.setAttribute('type','button');
-btn.setAttribute('value','copy url def');
+btn.setAttribute('value','copy url');
 //btn.setAttribute('style','padding: 0 2px; margin: 0 2px; color: #666;');
 
 var bilibili_btn_func = function(e) {
@@ -20,14 +20,16 @@ btn.addEventListener('click', bilibili_btn_func);
 
 var h = setInterval(function() {
   //console.log( document.querySelector('script[src$="nc-loader-0.12.0.min.js"]').toString());
-  if ( document.querySelector('script[src$="nc-loader-0.12.0.min.js"]') ) {
+  //if ( document.querySelector('script[src$="nc-loader-0.12.0.min.js"]') ) {
+  if ( document.querySelector('button.bilibili-player-iconfont') ) {
     console.info('video is ok');
     clearInterval(h);
     //var nr = document.createElement('p');
     //nr.textContent = document.querySelector('h1.video-title')['title'];
     //document.querySelector('div#viewbox_report').append(nr)
     //document.querySelector('h1').after(btn);
-    document.querySelector('span.rank').after(btn);
+    //document.querySelector('span.rank').after(btn);
+    document.querySelector('div.video-data').append(btn);
   }
 }, 300);
 
