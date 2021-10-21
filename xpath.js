9e0956b87161b2b8a67b6eb5b1f449a809653888
loaxsc <loaxs.chen@gmail.com>
2021-10-21 09:32:10 +0800

    upload xpath.js

---
+++ xpath.js
@@ -0,0 +1,10 @@
+var xpath = function(xpathToExecute){
+	var result = [];
+	var nodesSnapshot = document.evaluate(xpathToExecute, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
+	for ( var i=0 ; i < nodesSnapshot.snapshotLength; i++ ){
+		result.push( nodesSnapshot.snapshotItem(i) );
+	}
+	return result;
+}
+
+// vim: sw=2
