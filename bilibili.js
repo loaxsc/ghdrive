e63f72c2ff0d206dc122bc2fe06ad681065c103b
loaxsc <loaxs.chen@gmail.com>
2021-10-24 12:21:08 +0800

    upload bilibili.js

--- bilibili.js
+++ bilibili.js
@@ -41,38 +41,4 @@ var h = setInterval(function() {
   }
 }, 300);/**/
 
-//btn.setAttribute('id','bilibili_btn');
-//btn.setAttribute('type','button');
-//btn.setAttribute('value','copy url');
-////btn.setAttribute('style','padding: 0 2px; margin: 0 2px; color: #666;');
-
-//var bilibili_btn_func = function(e) {
-  //var url = location.toString();
-  //var dt = document.querySelector('div.video-data > span:nth-of-type(3)')
-				   //.innerText.split(' ')[0].replaceAll('-','');
-  //var title = document.querySelector('span.tit').innerText;
-  //var text = '`' + dt + '`' + title + '`' + url + '`';
-  //navigator.clipboard.writeText(text).then(function() {
-	  //console.log('Async: Copying to clipboard was successful!');
-  //}, function(err) {
-	  //console.error('Async: Could not copy text: ', err);
-  //});
-//}
-//btn.addEventListener('click', bilibili_btn_func);
-
-//var h = setInterval(function() {
-  ////console.log( document.querySelector('script[src$="nc-loader-0.12.0.min.js"]').toString());
-  ////if ( document.querySelector('script[src$="nc-loader-0.12.0.min.js"]') ) {
-  //if ( document.querySelector('button.bilibili-player-iconfont') ) {
-    //console.info('video is ok');
-    //clearInterval(h);
-    ////var nr = document.createElement('p');
-    ////nr.textContent = document.querySelector('h1.video-title')['title'];
-    ////document.querySelector('div#viewbox_report').append(nr)
-    ////document.querySelector('h1').after(btn);
-    ////document.querySelector('span.rank').after(btn);
-    //document.querySelector('div.video-data').append(btn);
-  //}
-//}, 300);
-
 // vim: fdm=marker sw=2
