73fb0b6ebc6e611cc2e83a0011a12c5dc34f4d2d
loaxsc <loaxs.chen@gmail.com>
2021-10-24 16:29:46 +0800

    upload weixin-html-shorten.js

--- weixin-html-shorten.js
+++ weixin-html-shorten.js
@@ -92,7 +92,7 @@ setTimeout(function() {
 	  var url = location.toString();
 	  var dt = document.querySelector('#publish_time').textContent.replaceAll('-','');
 	  var title = document.querySelector('#activity-name').innerText;
-	  var text = '`' + dt + '`' + title + '`' + url + '`';
+	  var text = '`' + title + '`' + url + '`' + dt + '`';
 	  navigator.clipboard.writeText(text).then(function() {
 		  console.log('Async: Copying to clipboard was successful!');
 	  }, function(err) {
