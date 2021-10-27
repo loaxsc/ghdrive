a4db691d18cc780eed522aae2aff8b77aa5c6fc0
loaxsc <loaxs.chen@gmail.com>
2021-10-27 17:25:34 +0800

    upload bilibili.js

--- bilibili.js
+++ bilibili.js
@@ -22,11 +22,25 @@ var bilibili_btn_func = function(e) {
 }
 btn.addEventListener('click', bilibili_btn_func);
 
+var btn_effect = function(btn_id) {
+  var btn_style = document.createElement('style');
+  btn_style.setAttribute('id', btn_id + '_effect');
+  btn_style.innerHTML = '#' + btn_id //+ ' { box-shadow: 2px 2px rgba(131, 204, 234, 0.5); }';
+    + ' { border: 2px solid skyblue; border-radius: 3px; }';
+  document.body.appendChild(btn_style);
+  console.log('function on');
+
+  setTimeout(function() {
+      document.body.removeChild(btn_style);
+  }, 500);
+}
+
 // Hotkeys, Shortcuts
 window.top.addEventListener('keydown', (e) => {
-  if (e.altKey && e.code == 'Digit0') {
+  if (!e.shiftKey && !e.ctrlKey && e.altKey && e.code == 'Digit0') {
     e.preventDefault();
     bilibili_btn_func(e);
+    btn_effect('bilibili_btn');
   }
   /*if ( e.shiftKey && e.altKey && e.code == 'Digit0' ) {
     e.preventDefault();
