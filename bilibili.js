34575d1749c189b7f293f7aabc211fd364f7ff00
loaxsc <loaxs.chen@gmail.com>
2021-10-28 09:34:38 +0800

    upload bilibili.js

--- bilibili.js
+++ bilibili.js
@@ -6,7 +6,8 @@ btn.setAttribute('value','copy url');
 //btn.setAttribute('style','padding: 0 2px; margin: 0 2px; color: #666;');
 
 var bilibili_btn_func = function(e) {
-  var url = location.toString();
+  var url = location.toString()
+                    .replace(/?.+$/);
   var dt = document.querySelector('div.video-data > span:nth-of-type(3)')
            .innerText.split(' ')[0].replaceAll('-','');
   var title = document.querySelector('span.tit').innerText
